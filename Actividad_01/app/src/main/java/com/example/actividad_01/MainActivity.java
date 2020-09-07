dff9ed1e88ee944dc5fa5f583b6bda62c937ab30
fernakasa <fernakasa@gmail.com>
2020-09-07 20:09:33 -0300

    Initial commit

---
+++ Actividad_01/app/src/main/java/com/example/actividad_01/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.actividad_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnSecondActivity = findViewById(R.id.btn_secondActivity);
+        Button btnThirdActivity = findViewById(R.id.btn_thirdActivity);
+
+        btnSecondActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnThirdActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
